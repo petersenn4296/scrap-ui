0aa84a20afccd25b4ed9f5bb1b6a77c22a1b5fa6
Peter Senn <psenn@vertafore.com>
2020-10-26 16:44:22 -0600

    got cards working

--- src/Recipe.jsx
+++ src/Recipe.jsx
@@ -1,25 +1,49 @@
 import React from 'react';
 import './Recipe.css'
+import ReactHtmlParser from 'react-html-parser';
 
 const Recipe = ({name, url, ingredients, instructions, notes}) => {
+  const sanitizeIngredient = ingredient => {
+    return (
+      <div>
+          {ingredient.split("\n").map((i,key) => {
+              if (key !== 0 && ingredients.length > 1) {
+                return (
+                  <div key={key} className='indented-ingredient'>
+                    {i}
+                  </div>)
+              } else {
+                return (
+                  <div key={key} >
+                    {i}
+                  </div>)
+              }
+
+          })}
+      </div>
+    )
+  }
+
   return (
     <div className="card">
         <div className="header">
           <p>{name}</p>
         </div>
             <div className="container">
-            <p>{url}</p>
+            <a href={url}>URL</a>
+            <h3>Ingredients</h3>
             <div>
               {/* TODO - add key prop */}
               {ingredients.map(ingredient => {
                 return (
                   <div>
-                    {ingredient}
+                    {sanitizeIngredient(ingredient)}
                   </div>
                 )
               })}
             </div>
-            <p>{instructions}</p>
+            <h3>Instructions</h3>
+            <div>{ReactHtmlParser(instructions)}</div>
             <p>{notes}</p>
         </div>
     </div>
